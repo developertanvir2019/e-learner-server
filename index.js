a95a7ee2521938fa94d09b3f7d718fa841cc5b4e
tanvir <tanvir101tk@gmail.com>
2022-10-25 20:24:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+app.use(cors());
+const courses = require('./courses.json');
+
+
+app.get('/', (req, res) => {
+    res.send(courses)
+});
+app.get('/courses', (req, res) => {
+    res.send(courses)
+});
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const selectedId = courses.find(n => n.category_id === id);
+    res.send(selectedId);
+});
+app.listen(port, () => {
+    console.log('news', port)
+})
+module.exports = app;
